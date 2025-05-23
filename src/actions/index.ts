0efd944283345fdc9076f0408cd322ca070051db
toreharose-bravo <toreha.ro.se.1149@gmail.com>
2025-05-24 00:13:44 +0900

    wip

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -2,15 +2,22 @@ import { defineAction } from "astro:actions";
 import { z } from "astro:schema";
 
 const formActionSchema = z.object({
-  name: z.string().min(1, { message: "名前を入力してください。" }),
-  email: z
-    .string()
-    .email({ message: "有効なメールアドレスを入力してください。" })
-    .optional(),
-  message: z
-    .string()
-    .min(10, { message: "メッセージは10文字以上で入力してください。" })
-    .optional(),
+  name: z.preprocess(
+    (val) => val || "",
+    z.string().min(1, { message: "名前を入力してください。" }),
+  ),
+  email: z.preprocess(
+    (val) => val || "",
+    z.string().email({ message: "有効なメールアドレスを入力してください。" }),
+  ),
+  message: z.preprocess(
+    (val) => val || "",
+    z
+      .string()
+      .min(10, {
+        message: "簡単で結構ですので、お問い合わせ内容を記載ください。",
+      }),
+  ),
 });
 
 export const server = {
