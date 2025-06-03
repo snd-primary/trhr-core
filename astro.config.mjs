0180503cb7b3cfea8be1a38dba0fa1c34f0bb9a8
toreharose-bravo <toreha.ro.se.1149@gmail.com>
2025-06-03 14:01:56 +0900

    wip

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
-
 import cloudflare from "@astrojs/cloudflare";
-
 import db from "@astrojs/db";
-
 const siteUrl = "https://trhr-core.dev";
 
 // https://astro.build/config
--- src/content.config.ts
+++ src/content.config.ts
@@ -1,18 +1,20 @@
 import { defineCollection, z } from "astro:content";
-
 import { glob } from "astro/loaders";
+import { rssSchema } from "@astrojs/rss";
 
 const works = defineCollection({
   loader: glob({ pattern: "**/*.md", base: "./src/data/works" }),
   schema: ({ image }) =>
-    z.object({
-      title: z.string(),
-      description: z.string(),
-      image: image(), // For the thumbnail
-      date: z.date(),
-      tags: z.array(z.string()), // For genre
-      // 'body' is not part of the schema, as it refers to the main content of the entry
-    }),
+    z.object(
+      {
+        title: z.string(),
+        description: z.string(),
+        image: image(), // For the thumbnail
+        date: z.date(),
+        tags: z.array(z.string()), // For genre
+      },
+      rssSchema,
+    ),
 });
 
 export const collections = {
